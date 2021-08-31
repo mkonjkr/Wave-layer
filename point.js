57ada666075c580df2cc9b021a1610c1b57705e3
unknown <mkonjkr@gmail.com>
2021-08-30 21:58:52 -0700

    one wave with line

---
+++ point.js
@@ -0,0 +1,16 @@
+export class Point {
+    constructor(index, x, y) {
+        this.x = x;
+        this.y = y;
+        this.fixedY = y;
+        this.speed = 0.05;
+        this.cur = index;
+        this.max = Math.random() * 100 + 150;
+    }
+
+    update() {
+        this.cur += this.speed;
+        // sin => return -1 ~ 1
+        this.y = this.fixedY + (Math.sin(this.cur) * this.max);
+    }
+}
